67133ea157a955c9dc2ac88c8fd9550614be181d
nerolis <yakumoft@gmail.com>
2017-09-15 16:02:43 +0300

    upd. Handle different message types..

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 var tmi = require('tmi.js');
+var $ = require('jQuery');
 
+// Options. 
 var options = {
     options:
        { debug: true },
@@ -13,11 +15,12 @@ var options = {
        },
     channels: ['nucl3arhc']
 };
-
+// Connect.
 var client = new tmi.client(options);
 client.connect();
 
 
+// An answers for phrases. 
 client.on('chat', function(channel, user, message, self) {
     if (message == 'посоветуй билд') {
         client.action('nucl3arhc', user['display-name'] + ', кок дизчардж!');
@@ -28,11 +31,35 @@ client.on('chat', function(channel, user, message, self) {
     if (message == 'почему гвард') {
         client.action('nucl3arhc', user['display-name'] + ', промахнулся в лабе Kappa');
     }
-    if (message === '!uptime') {
-        
+    if (message == 'брокен') {
+        client.action('nucl3arhc', user['display-name'] + ', FrankerZ');
     }
-});                                     
+    if (message == 'сколько реген ес') {
+        client.action('nucl3arhc', user['display-name'] + ', energy shield regen 1763.4');
+    }
+});    
 
+client.on("message", function (channel, userstate, message, self) {
+    // Don't listen to my own messages..
+    if (self) return;
+
+    // Handle different message types..
+    switch (userstate["message-type"]) {
+        case "action":
+            client.action('nucl3arhc', userstate['display-name'] + ', this is an action msg');
+            break;
+        case "chat":
+        // this
+            break;
+        case "whisper":
+            client.action('nucl3arhc', userstate['display-name'] + ', this is a whisper');
+            break;
+        default:
+            client.action('nucl3arhc', userstate['display-name'] + ', something else');
+            break;
+    }
+});
+
+// Start.
 client.on('connected', function(address, port) {
-    client.action('nucl3arhc', 'MrDestructoid');
 });                                                                                                 
