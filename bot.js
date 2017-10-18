c8dcf666a8393c452174885aca277558df6f5c34
nerolis <yakumoft@gmail.com>
2017-10-18 19:19:47 +0300

    upd

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 var tmi = require('tmi.js');
-var $ = require('jQuery');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(`<!DOCTYPE html>`);
+const $ = require('jQuery')(window);
 
 // Options. 
 var options = {
@@ -10,56 +13,41 @@ var options = {
          reconnect: true
         },
     identity: {
-         username: 'renelisz',
-         password: 'oauth:orjlr6h2rrl8sjgm1qxjkllhme7xp0'
+         username: 'qwhejuqwhne',
+         password: 'oauth:g4btluwtvdv0ouuk4yjyb50ke7t8dw'
        },
-    channels: ['nucl3arhc']
+    channels: ['lesstra', 'annieflowers']
 };
-// Connect.
+
 var client = new tmi.client(options);
 client.connect();
 
+client.on('join', function (channel, user, message) {
+    if (user === '808s808s808s') {
+        client.action('lesstra', `внимание, @808s808s808s зашел в чат`);
+    }
+});   
 
-// An answers for phrases. 
-client.on('chat', function(channel, user, message, self) {
-    if (message == 'посоветуй билд') {
-        client.action('nucl3arhc', user['display-name'] + ', кок дизчардж!');
-    }
-    if (message == 'почему окультист') {
-        client.action('nucl3arhc', user['display-name'] + ', промахнулся в лабе Kappa');
-    }
-    if (message == 'почему гвард') {
-        client.action('nucl3arhc', user['display-name'] + ', промахнулся в лабе Kappa');
-    }
-    if (message == 'брокен') {
-        client.action('nucl3arhc', user['display-name'] + ', FrankerZ');
-    }
-    if (message == 'сколько реген ес') {
-        client.action('nucl3arhc', user['display-name'] + ', energy shield regen 1763.4');
-    }
-});    
-
-client.on("message", function (channel, userstate, message, self) {
-    // Don't listen to my own messages..
-    if (self) return;
-
-    // Handle different message types..
-    switch (userstate["message-type"]) {
-        case "action":
-            client.action('nucl3arhc', userstate['display-name'] + ', this is an action msg');
-            break;
-        case "chat":
-        // this
-            break;
-        case "whisper":
-            client.action('nucl3arhc', userstate['display-name'] + ', this is a whisper');
-            break;
-        default:
-            client.action('nucl3arhc', userstate['display-name'] + ', something else');
-            break;
-    }
-});
-
-// Start.
 client.on('connected', function(address, port) {
+    let intervalTimer = setInterval(() => {
+        CheckOnlineStatus()
+    }, 60000); // Каждую минуту.
 });                                                                                                 
+
+CheckOnlineStatus = () => {
+    $.ajax({
+        url: `https://api.twitch.tv/kraken/streams/annieflowers`,
+        dataType: 'json',
+        headers: {
+            'Client-ID': 'j2cclgmrxhctqtee70qgm6uqg25cy4'
+        },
+        success: function (channel) {
+            if (channel["stream"] == null) {
+                console.log('Offline. After 60 sec, another parse.')
+            } else {
+                client.action('annieflowers', ` @annieflowers online, clear timeout`);
+                clearTimeout(intervalTimer);
+            }
+        }
+    });
+}
