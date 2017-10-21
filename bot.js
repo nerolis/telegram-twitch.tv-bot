f838225f1e0dec0daf41f260bda24e8274c6e0ab
nerolis <yakumoft@gmail.com>
2017-10-21 16:03:23 +0300

    implements reply from console to channel

--- bot.js
+++ bot.js
@@ -5,6 +5,9 @@ const { JSDOM } = jsdom;
 const { window } = new JSDOM(`<!DOCTYPE html>`);
 const $ = require('jQuery')(window);
 const fs = require('fs');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
 // Options. 
 var options = {
     options:
@@ -17,27 +20,41 @@ var options = {
          username: 'qwhejuqwhne',
          password: 'oauth:g4btluwtvdv0ouuk4yjyb50ke7t8dw'
        },
-    channels: ['lesstra', 'annieflowers']
+    channels: ['annieflowers', `qwhejuqwhne`]
 };
 
 var client = new tmi.client(options);
 client.connect();
 
+
+// Возможность отвечать из консоли в чат моего канала.
+process.stdin.on('data', function (text) {
+ if (text.substring(0, 4) === '.msg') {
+    reply(text);
+  }
+});
+
+// получился какой-то высер, но работает, пока оставлю.
+reply = (text) => {
+    client.action('qwhejuqwhne', text.slice(4))
+}
+
 client.on('join', function (channel, user, message) {
-    if (user === '808s808s808s') {
-        client.action('lesstra', `внимание, @808s808s808s зашел в чат`);
+    if (user !== 'qwhejuqwhne') {
+        console.log(user + channel)
     }
 });   
 
+// На коннекте заводим интервал на чек онлайн стрима.
 client.on('connected', function(address, port) {
     let intervalTimer = setInterval(() => {
         CheckOnlineStatus()
-    }, 60000); // Каждую минуту.
+    }, 60000); // Минуту.
 });                                                                                                 
 
-
-lastBroadcast = () => {
-fs.writeFile('lastBroadcast.txt', new Date().toLocaleString(), function (err) {
+// Создаем и обновляем блокнотик с датой последнего онлайна. 
+lastBroadcast = () => { 
+fs.writeFile('lastBroadcast.txt', new Date().toLocaleString(), function (err) { 
     if (err) 
         return console.log(err);
 });
@@ -53,17 +70,17 @@ CheckOnlineStatus = () => {
         success: function (channel) {
             if (channel["stream"] == null) {
                 console.log('Offline. After 60 sec, another parse.')
-                fs.readFile('lastBroadcast.txt', 'utf8', function (err,data) {
+                fs.readFile('lastBroadcast.txt', 'utf8', function (err, data) { // открываем файл и чекаем, дату, когда стрим был онлайн в последний раз.
                 if (err) {
                     return console.log(err);
                 }
-                    client.action('annieflowers', `last seen ${data}`);
+                    console.log(data) // Выводим  в консоль дату
                 });
 
             } else {
-                client.action('annieflowers', ` @annieflowers online, clear timeout.`);
-                lastBroadcast()
-                clearTimeout(intervalTimer);
+                // client.action('annieflowers', `online`);
+                lastBroadcast() // Если стрим онлайн, мы сохраняем последний онлайн(дату) в функции lastBroadcast 
+                clearTimeout(); // Если стрим онлайн, мы клирим(выключаем интервал)
             }
         }
     });
