2ca2665adbeb396407a9243bdb249f4303727a08
nerolis <yakumoft@gmail.com>
2018-04-30 23:31:53 +0300

    refactoring.

--- bot.js
+++ bot.js
@@ -1,87 +1,63 @@
-const tmi = require('tmi.js');
-const jsdom = require("jsdom");
-const mongodb = require('mongodb')
-const { JSDOM } = jsdom;
-const { window } = new JSDOM(`<!DOCTYPE html>`);
-const $ = require('jQuery')(window);
-const fs = require('fs');
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+import { options, proxy, token, target }            from './conf.js';
+import tmi                                          from 'tmi.js';
+import Telegraf                                     from 'telegraf';
+import SocksProxyAgent                              from 'socks-proxy-agent';
+const commandArgsMiddleware = require('./middleware');
 
-// Options. 
-var options = {
-    options:
-       { debug: true },
-    connection: {
-         cluster: 'aws',
-         reconnect: true
-        },
-    identity: {
-         username: 'qwhejuqwhne',
-         password: 'oauth:g4btluwtvdv0ouuk4yjyb50ke7t8dw'
-       },
-    channels: ['annieflowers', `qwhejuqwhne`]
-};
+const telegram   = new Telegraf(token, { telegram: { agent: new SocksProxyAgent('socks://88.198.202.64:1080') }});
+const twitch     = new tmi.client(options);
 
-var client = new tmi.client(options);
-client.connect();
+telegram.use(commandArgsMiddleware());
 
 
-// Возможность отвечать из консоли в чат моего канала.
-process.stdin.on('data', function (text) {
- if (text.substring(0, 4) === '.msg') {
-    reply(text);
-  }
+telegram.startPolling();
+twitch.connect();
+
+let subscribed = false;
+let init = {};
+
+twitch.on('connected', (address, port) => {
+    init.twitch = true;
+    console.log('Twitch = true');
 });
 
-// получился какой-то высер, но работает, пока оставлю.
-reply = (text) => {
-    client.action('qwhejuqwhne', text.slice(4))
-}
-
-client.on('join', function (channel, user, message) {
-    if (user !== 'qwhejuqwhne') {
-        console.log(user + channel)
-    }
-});   
-
-// На коннекте заводим интервал на чек онлайн стрима.
-client.on('connected', function(address, port) {
-    let intervalTimer = setInterval(() => {
-        CheckOnlineStatus()
-    }, 60000); // Минуту.
-});                                                                                                 
-
-// Создаем и обновляем блокнотик с датой последнего онлайна. 
-lastBroadcast = () => { 
-fs.writeFile('lastBroadcast.txt', new Date().toLocaleString(), function (err) { 
-    if (err) 
-        return console.log(err);
+telegram.telegram.getMe().then(() => {
+    init.twitch = true;
+    console.log(`Telegram = true`);
 });
 
+if (!init.twitch && init.telegram) {
+    throw new Error('Something wrong with connect');
+} else {
+    initTelegramCommands();
 }
-CheckOnlineStatus = () => {
-    $.ajax({
-        url: `https://api.twitch.tv/kraken/streams/annieflowers`,
-        dataType: 'json',
-        headers: {
-            'Client-ID': 'j2cclgmrxhctqtee70qgm6uqg25cy4'
-        },
-        success: function (channel) {
-            if (channel["stream"] == null) {
-                console.log('Offline. After 60 sec, another parse.')
-                fs.readFile('lastBroadcast.txt', 'utf8', function (err, data) { // открываем файл и чекаем, дату, когда стрим был онлайн в последний раз.
-                if (err) {
-                    return console.log(err);
-                }
-                    console.log(data) // Выводим  в консоль дату
-                });
 
-            } else {
-                // client.action('annieflowers', `online`);
-                lastBroadcast() // Если стрим онлайн, мы сохраняем последний онлайн(дату) в функции lastBroadcast 
-                clearTimeout(); // Если стрим онлайн, мы клирим(выключаем интервал)
-            }
+function initTelegramCommands() {
+    telegram.hears('!subscribe', ctx => {
+        if (!subscribed) {
+            subscribeOnChannel(ctx);
+        } else {
+            ctx.reply(`Already subscribed on ${options.channels} channels`);
         }
     });
-}
+};
+
+const subscribeOnChannel = ctx => {
+    subscribed = true;
+    ctx.reply(`Subscribed on ${options.channels} channels`);
+    
+    twitch.on("chat", (channel, userstate, message, self, user) => {
+        ctx.reply(`${userstate['display-name']} : ${message}`);
+    });
+};
+
+const checkTarget = userstate => {
+    if (userstate['display-name'] !== target)
+        return false
+
+    return true;
+};
+
+twitch.on('join', (channel, user, message) => {
+    if (user === target) console.log(`${target} joined the channel`);
+});
