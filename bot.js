3961839f2fe5627ab4e831d76cd1f57ef6de4fd8
nerolis <yakumoft@gmail.com>
2018-05-02 02:27:20 +0300

    refactoring

--- bot.js
+++ bot.js
@@ -2,8 +2,7 @@ import { options, token }                           from './conf.js';
 import tmi                                          from 'tmi.js';
 import Telegraf                                     from 'telegraf';
 import SocksProxyAgent                              from 'socks-proxy-agent';
-
-const commandArgsMiddleware = require('./middleware');
+import commandArgsMiddleware                        from './middleware';
 
 const Telegram   = new Telegraf(token, { telegram: { agent: new SocksProxyAgent('socks://88.198.202.64:1080') } });
 const Twitch     = new tmi.client(options);
@@ -11,47 +10,52 @@ const Twitch     = new tmi.client(options);
 Telegram.use(commandArgsMiddleware());
 Telegram.startPolling();
 
+initTelegramCommands();
+initTwitchListeners();
+
 let subscribers = {};
 
-initTelegramCommands();
-
 function initTelegramCommands() {
-        
-    Telegram.start(ctx => ctx.reply('Бот, который помогает отслеживать сообщения с twitch.tv. /join $channelname для того, чтоб начать пользоваться ботом.'));
- 
-    Telegram.command('commands', ctx => getCommandList(ctx));
+    Telegram.start(ctx =>
+         ctx.reply('/join $channelname. Join twitch channel.'));
+    
+    Telegram.command('commands', ctx =>
+         ctx.reply(`/subscribe - listen to all msges in joined channel. /stalk $channelname listen to msgs by specific user in joined channel`));
 
-    Telegram.command('join', ctx => {
-        const channelName = ctx.contextState.command.args;
-
-        joinTwitch(ctx, channelName);
-    });
+    Telegram.command('join', ctx => 
+        joinTwitch(ctx));
 
     Telegram.command('subscribe', ctx => {
         if (!subscribers.channels) {
-            subscribeOnChannel(ctx);
+            listenJoinedChannel(ctx);
         } else {
-            ctx.reply(`Already subscribed on ${options.channels} channel`);
+            ctx.reply(`Already listen to ${options.channels} channel`);
         }
     });
 
     Telegram.command('stalk', ctx => {
-        const nickname = ctx.contextState.command.args;
-
         if (!subscribers.nickname) {
-            subscribeOnTarget(ctx, nickname);
+            listenToSpecificUser(ctx);
         } else {
             ctx.reply(`Already stalk for ${nickname} channel`);
         }
     });
 };
 
-
-const getCommandList = ctx => {
-    ctx.reply(`/subscribe - подписка все сообщения текущего канала. /stalk $channelname подписка на все сообщения конкретного человека`);
+function initTwitchListeners() {
+    Twitch.on("chat", (channel, userstate, message, self, user) => {
+        if (message.split(' ')[0] === '!ask') {
+            if (Math.random() >= 0.5) {
+                Twitch.action(channel, userstate['display-name'] + ', Да.');
+            } else {
+                Twitch.action(channel, userstate['display-name'] + ', Нет.');
+            }
+        }
+    });
 };
 
-const subscribeOnTarget  = (ctx, nickname) => {
+const listenToSpecificUser  = ctx => {
+    const nickname = ctx.contextState.command.args;
     subscribers.nickname = true;
 
     ctx.reply(`Subscribed on ${nickname} messages`);
@@ -63,33 +67,24 @@ const subscribeOnTarget  = (ctx, nickname) => {
     });
 };
 
-const subscribeOnChannel  = ctx => {
+const listenJoinedChannel  = ctx => {
     subscribers.channels  = true;
-    ctx.reply(`Subscribed on ${options.channels} channel`);
+    ctx.reply(`Now listen to ${options.channels} channel`);
     
     Twitch.on("chat", (channel, userstate, message, self, user) => {
         ctx.reply(`${userstate['display-name']} : ${message}`);
     });
 };
 
-const joinTwitch = (ctx, channelName) => {
+const joinTwitch = ctx => {
+    const channelName = ctx.contextState.command.args;
     options.channels.push(channelName);
 
-    ctx.reply(`now listen to ${options.channels}. /commands`);
+    ctx.reply(`Now listen to ${options.channels}. /commands`);
 
     if (options.channels.length < 1) {
         ctx.reply(`Channel list is empty`);
     } else {
         Twitch.connect();
     }
-}
-
-Twitch.on("chat", (channel, userstate, message, self, user) => {
-    if (message.split(' ')[0] === '!ask') {
-        if (Math.random() >= 0.5) {
-            Twitch.action(channel, userstate['display-name'] + ', Да.');
-        } else {
-            Twitch.action(channel, userstate['display-name'] + ', Нет.');
-        }
-    }
-});
+};
